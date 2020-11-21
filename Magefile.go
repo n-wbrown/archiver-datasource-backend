a2f8b553b406a48e940473ca0428391482cb099e
n-wbrown <n-wbrown@users.noreply.github.com>
2020-11-20 19:03:10 -0800

    TST: add some basic testing structure, option for verbose testing

--- Magefile.go
+++ Magefile.go
@@ -6,11 +6,20 @@ import (
 	"fmt"
 	// mage:import
 	build "github.com/grafana/grafana-plugin-sdk-go/build"
+	"github.com/magefile/mage/sh"
 )
 
 // Hello prints a message (shows that you can define custom Mage targets).
 func Hello() {
 	fmt.Println("hello plugin developer!")
+}
+
+// vtest() runs go test but uses the verbose option and does not stop on failure 
+func Vtest() error {
+	if err := sh.RunV("go", "test", "-v", "./pkg/..."); err != nil {
+		return err
+	}
+	return nil
 }
 
 // Default configures the default target.
--- go.mod
+++ go.mod
@@ -4,5 +4,5 @@ go 1.14
 
 require (
 	github.com/grafana/grafana-plugin-sdk-go v0.79.0
-	github.com/magefile/mage v1.10.0 // indirect
+	github.com/magefile/mage v1.10.0
 )
