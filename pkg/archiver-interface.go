86add5c0c995c7aa4695bb09af33a9884c8b5efb
n-wbrown <n-wbrown@users.noreply.github.com>
2020-12-18 19:33:11 -0800

    WIP: work request JSON blob

--- pkg/archiver-interface.go
+++ pkg/archiver-interface.go
@@ -3,16 +3,16 @@ package main
 
 import ( 
 	"context"
-	// "encoding/json"
+	"encoding/json"
 	// "math/rand"
 	"net/http"
-	// "time"
+	"time"
 
 	"github.com/grafana/grafana-plugin-sdk-go/backend"
 	"github.com/grafana/grafana-plugin-sdk-go/backend/datasource"
 	"github.com/grafana/grafana-plugin-sdk-go/backend/instancemgmt"
 	"github.com/grafana/grafana-plugin-sdk-go/backend/log"
-	//"github.com/grafana/grafana-plugin-sdk-go/data"
+	"github.com/grafana/grafana-plugin-sdk-go/data"
 )
 
 func newArchiverDataSource() datasource.ServeOpts {
@@ -66,9 +66,44 @@ func (td *ArchiverDatasource) CheckHealth(ctx context.Context, req *backend.Chec
     }, nil
 }
 
+type archiverQueryModel struct {
+    Format string `json:"format"`
+}
+
 func (td *ArchiverDatasource) query(ctx context.Context, query backend.DataQuery) backend.DataResponse {
-    log.DefaultLogger.Debug("Executing Query")
+    log.DefaultLogger.Debug("Executing Query", query)
+    log.DefaultLogger.Debug("query JSON: ", query.JSON)
+    // Unmarshal the json into our queryModel
+    var qm archiverQueryModel
+
     response := backend.DataResponse{}
+
+    response.Error = json.Unmarshal(query.JSON, &qm)
+    if response.Error != nil {
+        return response
+    }
+
+    // Log a warning if 'Format' is empty
+    if qm.Format == "" {
+        log.DefaultLogger.Warn("format is empty. defaulting to time series")
+    }
+
+    // create data frame response
+    frame := data.NewFrame("response")
+
+    //add the time dimension
+    frame.Fields = append(frame.Fields,
+        data.NewField("time", nil, []time.Time{query.TimeRange.From, query.TimeRange.To}),
+    )
+
+    // add values 
+    frame.Fields = append(frame.Fields, 
+        data.NewField("values", nil, []int64{10, 20}),
+    )
+
+    // add the frames to the response
+    response.Frames = append(response.Frames, frame)
+
     return response
 }
 
