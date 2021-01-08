4cb7f7083f9d8491978c0d4414c1322b2b1aac8e
n-wbrown <n-wbrown@users.noreply.github.com>
2021-01-07 19:15:02 -0800

    WIP: experimental use of outside fiels

--- src/module.ts
+++ src/module.ts
@@ -1,8 +1,12 @@
 import { DataSourcePlugin } from '@grafana/data';
 import { DataSource } from './DataSource';
+import { ablater } from '../tst/TRK';
+import { testing_param } from './tst';
 import { ConfigEditor } from './ConfigEditor';
 import { QueryEditor } from './QueryEditor';
 import { MyQuery, MyDataSourceOptions } from './types';
+
+export const f = testing_param + ablater + 3;
 
 export const plugin = new DataSourcePlugin<DataSource, MyQuery, MyDataSourceOptions>(DataSource)
   .setConfigEditor(ConfigEditor)
