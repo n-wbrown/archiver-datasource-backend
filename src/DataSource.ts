c406ec97094ef0ddb5e421448854f5d4b9375b4d
n-wbrown <n-wbrown@users.noreply.github.com>
2021-01-08 12:11:47 -0800

    WIP: First extra-package import seems to be working

--- src/DataSource.ts
+++ src/DataSource.ts
@@ -1,6 +1,7 @@
 import { DataSourceInstanceSettings } from '@grafana/data';
 import { DataSourceWithBackend } from '@grafana/runtime';
 import { MyDataSourceOptions, MyQuery } from './types';
+import { AAQuery } from '../node_modules/sasaki77-archiver-appliance/src/types';
 
 export class DataSource extends DataSourceWithBackend<MyQuery, MyDataSourceOptions> {
   constructor(instanceSettings: DataSourceInstanceSettings<MyDataSourceOptions>) {
