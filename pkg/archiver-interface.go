a71b4043dd11470d604931aa782cb674c183f77d
n-wbrown <n-wbrown@users.noreply.github.com>
2021-01-20 16:26:53 -0800

    WIP: make progress on regex queries

--- pkg/archiver-interface.go
+++ pkg/archiver-interface.go
@@ -8,8 +8,9 @@ import (
 	"net/http"
     "net/url"
 	"time"
-    "reflect"
+    //"reflect"
     "strings"
+    "strconv"
     "io/ioutil"
 
 	"github.com/grafana/grafana-plugin-sdk-go/backend"
@@ -41,11 +42,11 @@ type ArchiverDatasource struct {
 
 func (td *ArchiverDatasource) QueryData(ctx context.Context, req *backend.QueryDataRequest) (*backend.QueryDataResponse, error) {
     // Structure defined by grafana-plugin-sdk-go. QueryData should unpack the req argument into individual queries.
-    log.DefaultLogger.Debug("Starting QueryData of newArchiverDataSource")
-    log.DefaultLogger.Debug("QueryData", "request", req)
-    log.DefaultLogger.Debug("QueryData.PluginContext", "PluginContext", req.PluginContext)
-    log.DefaultLogger.Debug("QueryData.PluginContext type", "PluginContext_type", reflect.TypeOf(req.PluginContext))
-    log.DefaultLogger.Debug("Plugintype.DataSourceInstanceSettings", "settings", req.PluginContext.DataSourceInstanceSettings.URL)
+    //log.DefaultLogger.Debug("Starting QueryData of newArchiverDataSource")
+    //log.DefaultLogger.Debug("QueryData", "request", req)
+    //log.DefaultLogger.Debug("QueryData.PluginContext", "PluginContext", req.PluginContext)
+    //log.DefaultLogger.Debug("QueryData.PluginContext type", "PluginContext_type", reflect.TypeOf(req.PluginContext))
+    //log.DefaultLogger.Debug("Plugintype.DataSourceInstanceSettings", "settings", req.PluginContext.DataSourceInstanceSettings.URL)
 
     // create response struct
     response := backend.NewQueryDataResponse()
@@ -117,27 +118,27 @@ func (td *ArchiverDatasource) query(ctx context.Context, query backend.DataQuery
     if response.Error != nil {
         return response
     }
-    log.DefaultLogger.Debug("query.JSON unmarshalled", "qm", qm)
-    log.DefaultLogger.Debug("qm.Target", "qm.Target", qm.Target)
+    // log.DefaultLogger.Debug("query.JSON unmarshalled", "qm", qm)
+    // log.DefaultLogger.Debug("qm.Target", "qm.Target", qm.Target)
 
 
     // let's extract all the relevant fields here:
 
     // data from query
-    log.DefaultLogger.Debug("query.TimeRange.From",    "value",    query.TimeRange.From)
-    log.DefaultLogger.Debug("query.TimeRange.To",      "value",    query.TimeRange.To)
-    log.DefaultLogger.Debug("query.QueryType",         "value",    query.QueryType)
-    log.DefaultLogger.Debug("query.MaxDataPoints",     "value",    query.MaxDataPoints)
-    //log.DefaultLogger.Debug("query.Interval",        "value",    query.Interval)
+    // log.DefaultLogger.Debug("query.TimeRange.From",    "value",    query.TimeRange.From)
+    // log.DefaultLogger.Debug("query.TimeRange.To",      "value",    query.TimeRange.To)
+    // log.DefaultLogger.Debug("query.QueryType",         "value",    query.QueryType)
+    // log.DefaultLogger.Debug("query.MaxDataPoints",     "value",    query.MaxDataPoints)
+    // log.DefaultLogger.Debug("query.Interval",        "value",    query.Interval)
     // data from unmarshaled JSON
     // log.DefaultLogger.Debug("qm.Datasource",        "value",    qm.Datasource)
-    log.DefaultLogger.Debug("qm.Target",               "value",    qm.Target)
-    log.DefaultLogger.Debug("qm.Regex",                "value",    qm.Regex)
+    // log.DefaultLogger.Debug("qm.Target",               "value",    qm.Target)
+    // log.DefaultLogger.Debug("qm.Regex",                "value",    qm.Regex)
     // log.DefaultLogger.Debug("qm.Operator",          "value",    qm.Operator)
-    //log.DefaultLogger.Debug("qm.Functions",          "value",    qm.Functions)
+    // log.DefaultLogger.Debug("qm.Functions",          "value",    qm.Functions)
 
     // data from original request's PluginContext
-    log.DefaultLogger.Debug("pluginctx.DataSourceInstanceSettings.URL", "value",    pluginctx.DataSourceInstanceSettings.URL)
+    // log.DefaultLogger.Debug("pluginctx.DataSourceInstanceSettings.URL", "value",    pluginctx.DataSourceInstanceSettings.URL)
 
     // Log a warning if 'Format' is empty
     if qm.Format == "" {
@@ -145,27 +146,35 @@ func (td *ArchiverDatasource) query(ctx context.Context, query backend.DataQuery
     }
 
     // make the query and compile the results into a singleData instance
-    queryUrl := BuildQueryUrl(query, pluginctx, qm)
-    singleResponse := archiverSingleQuery(queryUrl)
+    responseData := make([]singleData, 0)
+    if qm.Regex {
+        regexUrl := BuildRegexUrl(qm.Target, pluginctx)
+        data := archiverRegexQuery(regexUrl)
+        log.DefaultLogger.Debug("regex data", "value", data)
+    } else {
+        queryUrl := BuildQueryUrl(qm.Target, query, pluginctx, qm)
+        responseData = append(responseData, archiverSingleQuery(queryUrl))
+    }
 
-    // create data frame response
-    frame := data.NewFrame("response")
+    for _, singleResponse := range responseData {
+        // create data frame response
+        frame := data.NewFrame("response")
 
-    //add the time dimension
-    frame.Fields = append(frame.Fields,
-        data.NewField("time", nil, singleResponse.Times),
-    )
+        //add the time dimension
+        frame.Fields = append(frame.Fields,
+            data.NewField("time", nil, singleResponse.Times),
+        )
 
-    // add values 
-    frame.Fields = append(frame.Fields, 
-        data.NewField("values", nil, singleResponse.Values),
-    )
+        // add values 
+        frame.Fields = append(frame.Fields, 
+            data.NewField("values", nil, singleResponse.Values),
+        )
 
-    // add the frames to the response
-    response.Frames = append(response.Frames, frame)
-    
+        // add the frames to the response
+        response.Frames = append(response.Frames, frame)
+    }
 
-    //// TESTING BLOCK
+    //// TESTING BLOCK  
 
     // create data frame response
     frame2 := data.NewFrame("response")
@@ -189,7 +198,7 @@ func (td *ArchiverDatasource) query(ctx context.Context, query backend.DataQuery
     frame2.Fields = append(frame2.Fields,
         data.NewField("values", nil, newResponse.Values),
     )
-    
+
     // add the frames to the response
     response.Frames = append(response.Frames, frame2)
     //// TESTING BLOCK
@@ -209,7 +218,7 @@ func newArchiverDataSourceInstance(setting backend.DataSourceInstanceSettings) (
 	}, nil
 }
 
-func BuildQueryUrl(query backend.DataQuery, pluginctx backend.PluginContext, qm archiverQueryModel) string {
+func BuildQueryUrl(target string, query backend.DataQuery, pluginctx backend.PluginContext, qm archiverQueryModel) string {
     // Build the URL to query the archiver built from Grafana's configuration
     // Set some constants
     TIME_FORMAT := "2006-01-02T15:04:05.000-07:00"
@@ -228,15 +237,15 @@ func BuildQueryUrl(query backend.DataQuery, pluginctx backend.PluginContext, qm 
     pathBuilder.WriteString(JSON_DATA_URL)
     u.Path = pathBuilder.String()
 
-    location := time.Now().Location()
-    zone, offset := time.Now().Zone()
+    // location := time.Now().Location()
+    // zone, offset := time.Now().Zone()
     // location, _ := time.Location(zone)
     // query.TimeRange.From.l
 
-    log.DefaultLogger.Debug("TimeRange.From TZ", "TZ", query.TimeRange.From.Location())
-    log.DefaultLogger.Debug("TimeRange.Format", "TZ", query.TimeRange.From.Format(TIME_FORMAT))
-    log.DefaultLogger.Debug("TimeRange.In", "TZ", query.TimeRange.From.In(location))
-    log.DefaultLogger.Debug("TimeRange.To TZ", "TZ", query.TimeRange.To.Location())
+    //log.DefaultLogger.Debug("TimeRange.From TZ", "TZ", query.TimeRange.From.Location())
+    //log.DefaultLogger.Debug("TimeRange.Format", "TZ", query.TimeRange.From.Format(TIME_FORMAT))
+    //log.DefaultLogger.Debug("TimeRange.In", "TZ", query.TimeRange.From.In(location))
+    //log.DefaultLogger.Debug("TimeRange.To TZ", "TZ", query.TimeRange.To.Location())
 
     /*
     new_time_from := time.Date(
@@ -251,20 +260,20 @@ func BuildQueryUrl(query backend.DataQuery, pluginctx backend.PluginContext, qm 
     */
 
 
-    log.DefaultLogger.Debug("Local Timezone", "zone", zone)
-    log.DefaultLogger.Debug("Local Timezone", "offset", offset)
+    //log.DefaultLogger.Debug("Local Timezone", "zone", zone)
+    //log.DefaultLogger.Debug("Local Timezone", "offset", offset)
 
     // assemble the query of the URL and attach it to u
     query_vals :=  make(url.Values)
-    query_vals["pv"] = []string{qm.Target} 
+    query_vals["pv"] = []string{target} 
     query_vals["from"] = []string{query.TimeRange.From.Format(TIME_FORMAT)}
     query_vals["to"] = []string{query.TimeRange.To.Format(TIME_FORMAT)}
     query_vals["donotchunk"] = []string{""}
     u.RawQuery = query_vals.Encode()
 
     // Display the result
-    log.DefaultLogger.Debug("u", "value", u)
-    log.DefaultLogger.Debug("u.String", "value", u.String())
+    // log.DefaultLogger.Debug("u", "value", u)
+    // log.DefaultLogger.Debug("u.String", "value", u.String())
     return u.String()
 }
 
@@ -289,7 +298,7 @@ type ArchiverResponseModel struct {
     } `json:"data"`
 }
 
-func archiverSingleQuery( queryUrl string) singleData {
+func archiverSingleQuery(queryUrl string) singleData {
     // Take the unformatted response from the http GET request and turn it into rows of timeseries data
     var sD singleData
 
@@ -308,7 +317,7 @@ func archiverSingleQuery( queryUrl string) singleData {
         return sD
     }
 
-    log.DefaultLogger.Debug("Raw data", "value", string(jsonAsBytes))
+    // log.DefaultLogger.Debug("Raw data", "value", string(jsonAsBytes))
 
     // Convert received data to JSON
     var data []ArchiverResponseModel
@@ -318,7 +327,7 @@ func archiverSingleQuery( queryUrl string) singleData {
         return sD
     }
 
-    log.DefaultLogger.Debug("Data as JSON", "value", data)
+    // log.DefaultLogger.Debug("Data as JSON", "value", data)
 
     // Build output data block
     dataSize := len(data[0].Data)
@@ -341,6 +350,65 @@ func archiverSingleQuery( queryUrl string) singleData {
         }
         sD.Values[idx] = valCache
     }
-    log.DefaultLogger.Debug("singleData block", "Data", sD)
+    // log.DefaultLogger.Debug("singleData block", "Data", sD)
     return sD
+}
+
+func BuildRegexUrl(regex string, pluginctx backend.PluginContext) string {
+    // Construct the request URL for the regex search of PVs and return it as a string
+    REGEX_URL := "bpl/getMatchingPVs"
+    REGEX_MAXIMUM_MATCHES := 100
+
+    // Unpack the configured URL for the datasource and use that as the base for assembling the query URL
+    u, err := url.Parse(pluginctx.DataSourceInstanceSettings.URL)
+        if err != nil {
+        log.DefaultLogger.Warn("err", "err", err)
+    }
+
+    // amend the incomplete path
+    var pathBuilder strings.Builder
+    pathBuilder.WriteString(u.Path)
+    pathBuilder.WriteString("/")
+    pathBuilder.WriteString(REGEX_URL)
+    u.Path = pathBuilder.String()
+
+    // assemble the query of the URL and attach it to u
+    query_vals :=  make(url.Values)
+    query_vals["regex"] = []string{regex} 
+    query_vals["limit"] = []string{strconv.Itoa(REGEX_MAXIMUM_MATCHES)} 
+    u.RawQuery = query_vals.Encode()
+
+    // log.DefaultLogger.Debug("u.String", "value", u.String())
+    return u.String()
+}
+
+func archiverRegexQuery(queryUrl string) []string {
+    // Make the GET request  for the JSON list of matching PVs, parse it, and return a list of strings
+    var pvList []string
+
+    // Make the GET request
+    httpResponse, getErr := http.Get(queryUrl)
+    if getErr != nil {
+        log.DefaultLogger.Warn("Get request has failed", "Error", getErr)
+        return pvList
+    }
+
+    // Convert get request response to variable and close the file
+    jsonAsBytes, ioErr := ioutil.ReadAll(httpResponse.Body)
+    httpResponse.Body.Close()
+    if ioErr != nil {
+        log.DefaultLogger.Warn("Parsing of incoming data has failed", "Error", ioErr)
+        return pvList
+    }
+
+    // Convert received data to JSON
+    var data []string
+    jsonErr := json.Unmarshal(jsonAsBytes, &data)
+    if jsonErr != nil {
+        log.DefaultLogger.Warn("Conversion of incoming data to JSON has failed", "Error", jsonErr)
+        return pvList
+    }
+
+    return data
+
 }
