ae94a2db7fe395bb98423d39b181af6fd8c2dbe2
n-wbrown <n-wbrown@users.noreply.github.com>
2021-01-20 18:07:37 -0800

    WIP: start testing structure

--- pkg/archiver-interface_test.go
+++ pkg/archiver-interface_test.go
@@ -4,11 +4,27 @@ import (
     "testing"
 )
 
-func TestWorks(t *testing.T) {
-    // This function tests nothing
+func TestBuildQueryUrl(t *testing.T) {
 }
+
+func TestArchiverSingleQuery(t *testing.T) {
+}
+
+func TestArchiverSingleQueryParser(t *testing.T){
+}
+
+func TestBuildRegexUrl(t *testing.T){
+}
+
+func TestArchiverRegexQueryParser(t *testing.T){
+}
+
+
+
+
+
 
 func TestFails(t *testing.T) {
     // This function and always fails
-    t.Error("This is an example of test failure")
+    // t.Error("This is an example of test failure")
 }
